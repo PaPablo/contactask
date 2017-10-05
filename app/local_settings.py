2868a3cedb1c8d174dc79e730306d3600c4a17e5
LucianoFromTrelew <lucianoserruya@hotmail.com>
2017-10-05 00:51:21 -0300

    cosas, qcyo

---
+++ app/local_settings.py
@@ -0,0 +1,19 @@
+# Copiar y pegar esto en un local_settings.py
+import os
+
+# *****************************
+# Environment specific settings
+# *****************************
+
+# DO NOT use "DEBUG = True" in production environments
+DEBUG = True
+
+# DO NOT use Unsecure Secrets in production environments
+# Generate a safe one with:
+#     python -c "import os; print repr(os.urandom(24));"
+SECRET_KEY = '1a2s3d4f'
+
+# SQLAlchemy settings
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:postgres@localhost/contactask'
+SQLALCHEMY_TRACK_MODIFICATIONS = False    # Avoids a SQLAlchemy Warning
+
